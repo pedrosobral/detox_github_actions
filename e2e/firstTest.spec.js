f35781add781303d4b55ebbc01090ba415573af4
Sobral, Pedro H <pedro.h.sobral@accenture.com>
2020-02-21 13:01:05 -0300

    increase timeout

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,11 +1,14 @@
 describe('Example', () => {
   beforeEach(async () => {
-    console.log('\n\n\n\n\nBEFORE reload RELOAD');
+    console.log('\n\n\n\n\nBEFORE enable sync');
+    await device.enableSynchronization();
+    console.log('\n\n\n\n\nAFTER enable sync');
+    console.log('\n\n\n\n\nBEFORE reload');
     await device.reloadReactNative();
     console.log('\n\n\n\n\nAFTER RELOAD');
-    console.log('\n\n\n\n\nBEFORE SINC');
-    await device.enableSynchronization();
-    console.log('\n\n\n\n\nAFTER SINC');
+    // console.log('\n\n\n\n\nBEFORE SINC');
+    // await device.enableSynchronization();
+    // console.log('\n\n\n\n\nAFTER SINC');
   });
 
   it('should have welcome screen', async () => {
