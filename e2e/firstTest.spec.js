10f857df274ca4a854a4ac369ab32a374b8a0a4f
Sobral, Pedro H <pedro.h.sobral@accenture.com>
2020-02-21 13:30:04 -0300

    use launch app

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,15 +1,9 @@
 describe('Example', () => {
-  // beforeEach(async () => {
-  //   console.log('\n\n\n\n\nBEFORE enable sync');
-  //   // await device.enableSynchronization();
-  //   console.log('\n\n\n\n\nAFTER enable sync');
-  //   console.log('\n\n\n\n\nBEFORE reload');
-  //   // await device.reloadReactNative();
-  //   console.log('\n\n\n\n\nAFTER RELOAD');
-  //   // console.log('\n\n\n\n\nBEFORE SINC');
-  //   // await device.enableSynchronization();
-  //   // console.log('\n\n\n\n\nAFTER SINC');
-  // });
+  beforeEach(async () => {
+    console.log('\n\n\n\n\nBEFORE launch');
+    await device.launchApp();
+    console.log('\n\n\n\n\nAFTER lauch');
+  });
 
   it('should have welcome screen', async () => {
     await expect(element(by.label('Step One'))).toBeVisible();
